59983f9a776bfaf3c925c5a920ae5ca2c4f53876
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 23:28:59 -0600

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+
+GEONAMES_API_ENDPOINT = "http://api.geonames.org/"
+GEONAMES_USERNAME = "YOUR_USERNAME"  # Geonames의 사용자 이름을 여기에 입력하세요.
+
+
+def get_countries():
+    url = f"{GEONAMES_API_ENDPOINT}countryInfoJSON?username={GEONAMES_USERNAME}"
+    response = requests.get(url)
+    countries = response.json().get("geonames", [])
+    return {country["countryName"]: country["countryCode"] for country in countries}
+
+
+def get_states(country_code):
+    url = f"{GEONAMES_API_ENDPOINT}admin1CodeJSON?country={country_code}&username={GEONAMES_USERNAME}"
+    response = requests.get(url)
+    states = response.json().get("geonames", [])
+    return {state["name"]: state["adminCode1"] for state in states}
+
+
+def get_cities(country_code, state_code):
+    url = f"{GEONAMES_API_ENDPOINT}searchJSON?country={country_code}&adminCode1={state_code}&featureClass=P&username={GEONAMES_USERNAME}"
+    response = requests.get(url)
+    cities = response.json().get("geonames", [])
+    return [city["name"] for city in cities]
+
+
+st.title("Country, State, and City Selector")
+
+# 국가 선택
+countries = get_countries()
+selected_country = st.selectbox("Select a country", list(countries.keys()))
+
+# 주 선택
+states = get_states(countries[selected_country])
+selected_state = st.selectbox("Select a state", list(states.keys()))
+
+# 도시 선택
+cities = get_cities(countries[selected_country], states[selected_state])
+selected_city = st.selectbox("Select a city", cities)
+
+# 결과 출력
+st.write(
+    f"You selected: {selected_country} > {selected_state} > {selected_city}")
