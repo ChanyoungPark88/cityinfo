e15fe2fe7507e3c1d5c2807417507f80ede70bac
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-31 01:02:08 -0600

    first commit

--- functions/functions.py
+++ functions/functions.py
@@ -4,41 +4,10 @@ from library.libraries import *
 BASE_URL = os.environ.get("BASE_URL")
 
 
-def get_available_countries():
-    """Available countries for the app."""
-    return [country.name for country in pycountry.countries if country.name in ["Canada", "United States"]]
-
-
 def get_country_info(country_name):
-    if country_name not in ["Canada", "United States"]:
-        return []
-
     params = {
         "country": country_name,
         "format": "json"
     }
     response = requests.get(BASE_URL, params=params)
     return response.json()
-
-
-def get_state_info(country_name):
-    params = {
-        "country": country_name,
-        "polygon_geojson": 1,
-        "format": "json"
-    }
-    response = requests.get(BASE_URL, params=params)
-    st.write(response.json())
-    states = [place for place in response.json() if place['type'] == 'state']
-    return states
-
-
-def get_city_info(country_name, state_name):
-    params = {
-        "country": country_name,
-        "state": state_name,
-        "format": "json"
-    }
-    response = requests.get(BASE_URL, params=params)
-    cities = [place for place in response.json() if place['type'] == 'city']
-    return cities
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,9 @@ from functions.functions import *
 
 st.title("Country, State, and City Selector using Nominatim API")
 
-countries = get_available_countries()
-selected_country = st.selectbox("Select a country", countries)
+selected_country = st.selectbox("Select a country")
 
 # 선택한 나라의 정보를 가져옵니다.
 country_info = get_country_info(selected_country)
 st.write(f"Selected Country: {selected_country}")
 st.write(country_info)
-
-# 선택한 나라의 주 정보를 가져옵니다.
-state_info = get_state_info(selected_country)
-st.write("States in the selected country:")
-
-# 주 정보를 출력합니다.
-for state in state_info:
-    st.write(state['name'])
-
-
-# if selected_country:
-#     # 주 선택
-#     states = get_state_info(selected_country)
-#     state_names = [state["display_name"].split(",")[0] for state in states]
-#     selected_state = st.selectbox("Select a state", state_names)
-#     st.write(selected_state)
-#     if selected_state:
-#         # 도시 선택
-#         cities = get_city_info(selected_country, selected_state)
-#         city_names = [city["display_name"].split(",")[0] for city in cities]
-#         selected_city = st.selectbox("Select a city", city_names)
-
-#         if selected_city:
-#             # 결과 출력
-#             st.write(
-#                 f"You selected: {selected_country} > {selected_state} > {selected_city}")
