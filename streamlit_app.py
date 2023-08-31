2f3362a5cf23e3b29bb53ab926cd8236f2428323
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 23:37:42 -0600

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,26 @@ import streamlit as st
 import requests
 import os
 
-GEONAMES_API_ENDPOINT = os.environ.get(GEONAMES_API_ENDPOINT)
-GEONAMES_USERNAME = os.environ.get(GEONAMES_USERNAME)
+API_ENDPOINT = os.environ.get("GEONAMES_API_ENDPOINT")
+USERNAME = os.environ.get("GEONAMES_USERNAME")
 
 
 def get_countries():
-    url = f"{GEONAMES_API_ENDPOINT}countryInfoJSON?username={GEONAMES_USERNAME}"
+    url = f"{API_ENDPOINT}countryInfoJSON?username={USERNAME}"
     response = requests.get(url)
     countries = response.json().get("geonames", [])
     return {country["countryName"]: country["countryCode"] for country in countries}
 
 
 def get_states(country_code):
-    url = f"{GEONAMES_API_ENDPOINT}admin1CodeJSON?country={country_code}&username={GEONAMES_USERNAME}"
+    url = f"{API_ENDPOINT}admin1CodeJSON?country={country_code}&username={USERNAME}"
     response = requests.get(url)
     states = response.json().get("geonames", [])
     return {state["name"]: state["adminCode1"] for state in states}
 
 
 def get_cities(country_code, state_code):
-    url = f"{GEONAMES_API_ENDPOINT}searchJSON?country={country_code}&adminCode1={state_code}&featureClass=P&username={GEONAMES_USERNAME}"
+    url = f"{API_ENDPOINT}searchJSON?country={country_code}&adminCode1={state_code}&featureClass=P&username={USERNAME}"
     response = requests.get(url)
     cities = response.json().get("geonames", [])
     return [city["name"] for city in cities]
