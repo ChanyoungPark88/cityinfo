66e5750d5ba318dbad45e073a0ecb93d187e4f13
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-31 01:19:39 -0600

    first commit

--- functions/functions.py
+++ functions/functions.py
@@ -1,22 +1,34 @@
 from library.libraries import *
 
+headers = {
+    "User-Agent": "Get Location Info"  # 앱의 이름을 지정
+}
 
-BASE_URL = os.environ.get("BASE_URL")
+SEARCH_URL = os.environ.get("SEARCH_URL")
+LOOKUP_URL = os.environ.get("LOOKUP_URL")
 
 
-def get_available_countries():
-    """Available countries for the app."""
-    return [country.name for country in pycountry.countries]
-
-
-def get_country_info(country_name):
+def get_osm_id_for_country(country_name, SEARCH_URL):
     params = {
-        "country": country_name,
+        "q": country_name,
+        "format": "json",
+        "limit": 1,  # 첫 번째 결과만 반환
+        "countrycodes": country_name
+    }
+    response = requests.get(SEARCH_URL, params=params, headers=headers)
+    if response.status_code == 200 and response.json():
+        return response.json()[0]['osm_id']
+    else:
+        return None
+
+
+def get_detailed_info_by_osm_id(osm_id, LOOKUP_URL):
+    params = {
+        "osm_ids": f"R{osm_id}",  # R은 관계(Relation)를 의미
         "format": "json"
     }
-    headers = {
-        "User-Agent": "getlocinfo"  # 앱의 이름을 지정
-    }
-    response = requests.get(BASE_URL, params=params, headers=headers)
-    st.write(response.json())
-    return response.json()
+    response = requests.get(LOOKUP_URL, params=params, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,24 @@
 from library.libraries import *
 from functions.functions import *
 
-st.title("Get Location Info")
 
-countries = get_available_countries()
-selected_country = st.selectbox("Select Country", countries)
-country_info = get_country_info(selected_country)
-st.write(f"You selected {country_info}")
+st.title("Country, State, and City Selector using Nominatim API")
+
+# 국가 선택
+countries = ["United States", "Canada"]  # 예시로 두 개의 국가만 사용
+selected_country = st.selectbox("Select a country", countries)
+osm_id_country = get_osm_id_for_country(selected_country, SEARCH_URL)
+
+# 주 선택
+if osm_id_country:
+    state_info = get_detailed_info_by_osm_id(osm_id_country, LOOKUP_URL)
+    if state_info:
+        states = [info['display_name']
+                  for info in state_info if info['type'] == 'state']
+        selected_state = st.selectbox("Select a state", states)
+
+        # 도시 선택
+        cities = [info['display_name'] for info in state_info if info['type']
+                  == 'city' and selected_state in info['display_name']]
+        if cities:
+            st.selectbox("Select a city", cities)
