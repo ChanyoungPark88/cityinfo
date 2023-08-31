828af3bdb95b1040194b69f8c25e902aaaed05a0
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-31 01:55:08 -0600

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 from library.libraries import *
 from functions.functions import *
 
-country_names = [country.name for country in pycountry.countries]
+# Filter the countries you want to display
+country_names = [country.name for country in pycountry.countries if country.name in [
+    "Canada", "United States"]]
 
 selected_country = st.selectbox("Select a country", country_names)
 st.write(f"You selected: {selected_country}")
