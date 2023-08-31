b55fcdf9432f42b544444b2d8e1e474f78e20694
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 23:42:30 -0600

    first commit

---
+++ functions/functions.py
@@ -0,0 +1,26 @@
+from library.libraries import *
+
+
+API_ENDPOINT = os.environ.get("GEONAMES_API_ENDPOINT")
+USERNAME = os.environ.get("GEONAMES_USERNAME")
+
+
+def get_countries():
+    url = f"{API_ENDPOINT}countryInfoJSON?username={USERNAME}"
+    response = requests.get(url)
+    countries = response.json().get("geonames", [])
+    return {country["countryName"]: country["countryCode"] for country in countries}
+
+
+def get_states(country_code):
+    url = f"{API_ENDPOINT}admin1CodeJSON?country={country_code}&username={USERNAME}"
+    response = requests.get(url)
+    states = response.json().get("geonames", [])
+    return {state["name"]: state["adminCode1"] for state in states}
+
+
+def get_cities(country_code, state_code):
+    url = f"{API_ENDPOINT}searchJSON?country={country_code}&adminCode1={state_code}&featureClass=P&username={USERNAME}"
+    response = requests.get(url)
+    cities = response.json().get("geonames", [])
+    return [city["name"] for city in cities]
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,5 @@
-import streamlit as st
-import requests
-import os
-
-API_ENDPOINT = os.environ.get("GEONAMES_API_ENDPOINT")
-USERNAME = os.environ.get("GEONAMES_USERNAME")
-
-
-def get_countries():
-    url = f"{API_ENDPOINT}countryInfoJSON?username={USERNAME}"
-    response = requests.get(url)
-    countries = response.json().get("geonames", [])
-    return {country["countryName"]: country["countryCode"] for country in countries}
-
-
-def get_states(country_code):
-    url = f"{API_ENDPOINT}admin1CodeJSON?country={country_code}&username={USERNAME}"
-    response = requests.get(url)
-    states = response.json().get("geonames", [])
-    return {state["name"]: state["adminCode1"] for state in states}
-
-
-def get_cities(country_code, state_code):
-    url = f"{API_ENDPOINT}searchJSON?country={country_code}&adminCode1={state_code}&featureClass=P&username={USERNAME}"
-    response = requests.get(url)
-    cities = response.json().get("geonames", [])
-    return [city["name"] for city in cities]
-
+from library.libraries import *
+from functions.functions import *
 
 st.title("Country, State, and City Selector")
 
