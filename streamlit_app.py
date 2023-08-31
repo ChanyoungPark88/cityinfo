ddc4a60fa3e6fb3b84c867f2a04b6c991cfdf64f
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-31 01:53:23 -0600

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,7 @@
 from library.libraries import *
 from functions.functions import *
 
-st.title("Country, State, and City Selector using Nominatim API")
+country_names = [country.name for country in pycountry.countries]
 
-# Dummy list for demonstration. This should be replaced by a real country list.
-country_list = ["USA", "Germany", "France", "South Korea"]
-selected_country = st.selectbox("Select a country", country_list)
-
-# Get OSM id and OSM type of the selected country
-osm_id, osm_type = get_country_osm_id(selected_country)
-
-# If OSM id and OSM type exist, then fetch country details
-if osm_id and osm_type:
-    country_details = get_state_and_city_details(osm_id, osm_type)
-else:
-    country_details = []
-
-if country_details:
-    states = [detail['address']['state']
-              for detail in country_details if 'address' in detail and 'state' in detail['address']]
-
-    selected_state = st.selectbox("Select a state", states)
-
-    cities = [detail['address']['city']
-              for detail in country_details if 'address' in detail and 'city' in detail['address']]
-
-    selected_city = st.selectbox("Select a city", cities)
-    st.write(
-        f"You selected: {selected_country} -> {selected_state} -> {selected_city}")
-else:
-    st.write("No details found for the selected country.")
+selected_country = st.selectbox("Select a country", country_names)
+st.write(f"You selected: {selected_country}")
