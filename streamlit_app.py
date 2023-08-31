a923a50c921e9dfb7a2c3a2488a3a9adb6d7c81f
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 23:45:59 -0600

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,23 +7,18 @@ st.title("Country, State, and City Selector")
 countries = get_countries()
 selected_country = st.selectbox("Select a country", list(countries.keys()))
 
-# 국가를 선택한 경우 주의 목록 로드
-if selected_country in countries:
+if selected_country:
+    # 주 선택
     states = get_states(countries[selected_country])
-else:
-    states = {}
+    selected_state = st.selectbox("Select a state", list(states.keys()))
 
-selected_state = st.selectbox("Select a state", list(states.keys()))
+    if selected_state:
+        # 도시 선택
+        cities = get_cities(
+            countries[selected_country], states[selected_state])
+        selected_city = st.selectbox("Select a city", cities)
 
-# 주를 선택한 경우 도시의 목록 로드
-if selected_state in states:
-    cities = get_cities(countries[selected_country], states[selected_state])
-else:
-    cities = []
-
-selected_city = st.selectbox("Select a city", cities)
-
-# 결과 출력
-if selected_country and selected_state and selected_city:
-    st.write(
-        f"You selected: {selected_country} > {selected_state} > {selected_city}")
+        if selected_city:
+            # 결과 출력
+            st.write(
+                f"You selected: {selected_country} > {selected_state} > {selected_city}")
