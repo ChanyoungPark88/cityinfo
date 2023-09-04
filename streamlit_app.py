9798cf4c8b4a4e5418d7dd4c9095c5f827b4df66
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2023-09-03 21:08:06 -0600

    Add docstrings

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ if selected_country_name != "Select a country":
     if selected_state_name != "Select a state":
         selected_state_code_temp = next(
             state["code"] for state in state_data if state["name"] == selected_state_name)
-selected_state_code = selected_state_code_temp.lower() if isinstance(
-    selected_state_code_temp, str) else selected_state_code_temp
+        selected_state_code = selected_state_code_temp.lower() if isinstance(
+            selected_state_code_temp, str) else selected_state_code_temp
 
 # 선택된 주/지방의 이름을 주/지방 코드로 변환합니다.
 # selected_state_code = next(
