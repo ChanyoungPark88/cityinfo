e1d5301bdb537948a9672c12f6e08bc15921f964
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2023-09-03 23:46:50 -0600

    Site structure updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,7 @@ from function.functions import (
 
 st.title("Zillow Search URL Generator using Country, State, and City")
 
-# pycountry를 통해 국가 리스트를 가져옵니다.
-country_list = ["Canada", "United States"]  # 현재 예시에서는 두 국가만 사용합니다.
+country_list = ["Canada", "United States"]
 selected_country_name = st.selectbox(
     "Select a country", ["Select a country"] + country_list)
 
@@ -25,25 +24,38 @@ elif selected_country_name == "Canada":
     if data_frame is not None:
         # 주 선택 드롭다운 생성
         provinces = ["Select a province"] + \
-            get_provinces_from_canada(data_frame)
+            sorted(get_provinces_from_canada(data_frame), reverse=True)
         selected_province = st.selectbox("Select a province", provinces)
 
         # 선택된 주에 해당하는 도시 선택 드롭다운 생성
         if selected_province != "Select a province":
             cities = ["Select a city"] + \
-                get_cities_from_province(data_frame, selected_province)
+                sorted(get_cities_from_province(
+                    data_frame, selected_province), reverse=True)
             selected_city = st.selectbox("Select a city", cities)
+
+            if selected_city != "Select a city":
+                province_id = data_frame[data_frame["province_name"]
+                                         == selected_province].iloc[0]['province_id']
+                st.write(f"{selected_city}-{province_id}")
 
 elif selected_country_name == "United States":
     FILENAME = "uscities_selected.csv"
     data_frame = download_file_from_gcs(FILENAME, storage_client)
     if data_frame is not None:
         # 주(State) 선택 드롭다운 생성
-        states = ["Select a state"] + get_states_from_usa(data_frame)
+        states = ["Select a state"] + \
+            sorted(get_states_from_usa(data_frame), reverse=True)
         selected_state = st.selectbox("Select a state", states)
 
         # 선택된 주(State)에 해당하는 도시 선택 드롭다운 생성
         if selected_state != "Select a state":
             cities = ["Select a city"] + \
-                get_cities_from_state(data_frame, selected_state)
+                sorted(get_cities_from_state(
+                    data_frame, selected_state), reverse=True)
             selected_city = st.selectbox("Select a city", cities)
+
+            if selected_city != "Select a city":
+                state_id = data_frame[data_frame["state_name"]
+                                      == selected_state].iloc[0]['state_id']
+                st.write(f"{selected_city}-{state_id}")
