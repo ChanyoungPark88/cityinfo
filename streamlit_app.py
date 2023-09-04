49691714b11e84213d6d4a881fd2688d6b0b9b1f
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2023-09-04 02:52:37 -0600

    For USA

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ based on the user's country, state, and city selections.
 from library.libraries import st
 from function.functions import (
     gcs_connect, download_file_from_gcs,
-    get_provinces_from_canada, get_states_from_usa,
-    get_cities_from_province, get_cities_from_state,
+    get_states_from_usa,
+    get_cities_from_state,
     generate_zillow_url
 )
 
 st.title("Zillow Search URL Generator using Country, State, and City")
 
-country_list = ["Canada", "United States"]
+country_list = ["United States"]
 selected_country_name = st.selectbox(
     "Select a country", ["Select a country"] + country_list)
 
@@ -19,34 +19,8 @@ storage_client = gcs_connect()
 if not storage_client:
     st.error("Failed to connect to Google Cloud Storage.")
 
-elif selected_country_name == "Canada":
-    FILENAME = "canadacities_selected.csv"
-    data_frame = download_file_from_gcs(FILENAME, storage_client)
-    if data_frame is not None:
-        # 주 선택 드롭다운 생성
-        provinces = ["Select a province"] + \
-            sorted(get_provinces_from_canada(data_frame))
-        selected_province = st.selectbox("Select a province", provinces)
-
-        # 선택된 주에 해당하는 도시 선택 드롭다운 생성
-        if selected_province != "Select a province":
-            cities = ["Select a city"] + \
-                sorted(get_cities_from_province(data_frame, selected_province))
-            selected_city = st.selectbox("Select a city", cities)
-
-            if selected_city != "Select a city":
-                city_data = data_frame[data_frame["city"]
-                                       == selected_city].iloc[0]
-                city_lat = city_data['lat']
-                city_lng = city_data['lng']
-                province_id = city_data['province_id']
-
-                zillow_url = generate_zillow_url(
-                    selected_city, province_id, city_lat, city_lng)
-                st.write(f"Zillow URL: {zillow_url}")
-
 elif selected_country_name == "United States":
-    FILENAME = "uscities_selected.csv"
+    FILENAME = "merged_usacities_data.csv"
     data_frame = download_file_from_gcs(FILENAME, storage_client)
     if data_frame is not None:
         # 주(State) 선택 드롭다운 생성
